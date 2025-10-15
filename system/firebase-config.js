6a988195884e449751ae11014899647eeb9e9f7a
Berat Akgök <142182016+Berat-365@users.noreply.github.com>
2025-10-15 21:20:52 +0300

    Add files via upload

---
+++ system/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBlFcY15LBgnKLPW-9w9XqaFPc5rrO42Ns",
+    authDomain: "fluxo-ef0bd.firebaseapp.com",
+    projectId: "fluxo-ef0bd",
+    storageBucket: "fluxo-ef0bd.firebasestorage.app",
+    messagingSenderId: "768814415446",
+    appId: "1:768814415446:web:22ccdf701865efb28845f7",
+    measurementId: "G-0EH1BLE91C"
+};
